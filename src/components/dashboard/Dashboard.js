3131dd8e6c80e0542f6cb51cead5384f9b061aba
Gaylord Carrillo C <gcarricaballero3@hotmail.com>
2023-08-26 11:47:43 -0500

    First commit of the project

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./__styles__/index.css";
+
+const BodyDashboard = () => {
+
+    return (
+        <div className='main-wrapped'>
+            <h1> Hola Mundo </h1>
+        </div>
+    )
+}
+
+export default BodyDashboard   
