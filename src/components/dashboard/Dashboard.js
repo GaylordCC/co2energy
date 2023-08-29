9726f7363300df768d52a275a02b8d575a5cd8ac
Gaylord Carrillo C <gcarricaballero3@hotmail.com>
2023-08-29 10:08:36 -0500

    Add of hover's property tothe  button component

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -135,12 +135,12 @@ const BodyDashboard = () => {
                             <div className='energy-management'>
                                 <img className='im-management' src={energetica} />
                                 <p className='text-management'> Gestión Energética </p>
-                                <button className='button-more'> Ver más </button>
+                                <button className='buttonmore'> Ver más </button>
                             </div>
                             <div className='emissions-management'>
                                 <img className='im-management' src={huella} />
                                 <p className='text-management'> Gestión de Emisiones </p>
-                                <button className='button-more'> Ver más </button>
+                                <button className='buttonmore'> Ver más </button>
                             </div>
                         </div>
 
