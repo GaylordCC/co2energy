762bbbb220ff42e22a1120223f57d182b38b991f
Gaylord Carrillo C <gcarricaballero3@hotmail.com>
2023-09-05 11:27:29 -0500

    Ajust of management activities component

--- src/components/service/Service.js
+++ src/components/service/Service.js
@@ -6,15 +6,15 @@ import huella from "./__style__/images/huella.png";
 const BodyService = () => {
 
     return (
-        <div className='main-wrapped'>
+        <div className='service-wrapped'>
             <div className='management-services'>
                 <div className='energy-management'>
-                    <img className='im-management' src={energetica} />
+                    {/* <img className='im-management' src={energetica} /> */}
                     <p className='text-management'> Gestión Energética </p>
                     <button className='buttonmore'> Ver más </button>
                 </div>
                 <div className='emissions-management'>
-                    <img className='im-management' src={huella} />
+                    {/* <img className='im-management' src={huella} /> */}
                     <p className='text-management'> Gestión de Emisiones </p>
                     <button className='buttonmore'> Ver más </button>
                 </div>
