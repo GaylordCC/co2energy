d06ce82e683016482c855cdfa6086a37b2c710d3
Gaylord Carrillo C <gcarricaballero3@hotmail.com>
2023-09-04 23:05:26 -0500

    Implementation of sticky position for the header component

--- src/components/service/Service.js
+++ src/components/service/Service.js
@@ -7,20 +7,18 @@ const BodyService = () => {
 
     return (
         <div className='main-wrapped'>
-            <a id="Serv">
-                <div className='management-services'>
-                    <div className='energy-management'>
-                        <img className='im-management' src={energetica} />
-                        <p className='text-management'> Gestión Energética </p>
-                        <button className='buttonmore'> Ver más </button>
-                    </div>
-                    <div className='emissions-management'>
-                        <img className='im-management' src={huella} />
-                        <p className='text-management'> Gestión de Emisiones </p>
-                        <button className='buttonmore'> Ver más </button>
-                    </div>
+            <div className='management-services'>
+                <div className='energy-management'>
+                    <img className='im-management' src={energetica} />
+                    <p className='text-management'> Gestión Energética </p>
+                    <button className='buttonmore'> Ver más </button>
                 </div>
-            </a>
+                <div className='emissions-management'>
+                    <img className='im-management' src={huella} />
+                    <p className='text-management'> Gestión de Emisiones </p>
+                    <button className='buttonmore'> Ver más </button>
+                </div>
+            </div>
         </div>
     )
 }
