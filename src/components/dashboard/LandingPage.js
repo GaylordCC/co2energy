a823bdd960927a3abbc588a8d5e2ca01b51f6fd7
GaylordCC <gcarricaballero3@hotmail.com>
2023-10-14 19:48:05 -0500

    Implementation of responsive method for the footer, header, contact form and home page component

--- src/components/dashboard/LandingPage.js
+++ src/components/dashboard/LandingPage.js
@@ -40,7 +40,7 @@ const BodyLandingPage = () => {
                     </h2>
                 </div>
                 <div className='scroll-video' onClick={() => window.location.replace("/#AU")}>
-                    <svg width="100px" height="100px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg class="svg-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C22 4.92893 22 7.28595 22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12Z" stroke="white" stroke-width="0.4"/>
                         <path d="M9 8.5L12 11.5L15 8.5" stroke="white" stroke-width="1.7" stroke-linecap="round" stroke-linejoin="round"/>
                         <path d="M9 12.5L12 15.5L15 12.5" stroke="white" stroke-width="1.7" stroke-linecap="round" stroke-linejoin="round"/>
