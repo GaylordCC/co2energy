c0657c0d8182a02fb65faf7256c881cb4144150f
GaylordCC <gcarricaballero3@hotmail.com>
2023-10-18 14:12:08 -0500

    Implementation of a modal, to show the confirmation of sending the form information

--- src/components/contactForm/Contact.js
+++ src/components/contactForm/Contact.js
@@ -1,39 +1,51 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import "./__style__/index.css";
 
 
 export default function Contact() {
     const form = useRef();
+    const [message, setMessage] = useState('');
 
     const sendEmail = (e) => {
-      e.preventDefault();
+        e.preventDefault();
   
-      emailjs.sendForm('service_1fs0vwv', 'template_pcsyv6r', e.target, '93S_coT7nBbX8AcQ8')
+        emailjs.sendForm('service_1fs0vwv', 'template_pcsyv6r', e.target, '93S_coT7nBbX8AcQ8')
         .then((result) => {
-            window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior) 
+            setMessage('Mensaje enviado con éxito');
+            form.current.reset();
             console.log(result.text);
             console.log("message sent");
         }, (error) => {
+            setMessage('Error al enviar el mensaje');
             console.log(error.text);
         });
     };
 
+
     return (
-        <form className='form' ref={form} onSubmit={sendEmail}>
-            <label>Nombre</label>
-            <input type="text" name="user_name" />
-            <label>Compañia</label>
-            <input type="text" name="company_name" />
-            <label>Email</label>
-            <input type="email" name="user_email" />
-            <label>Teléfono</label>
-            <input type="number" name="user_phone" />
-            {/* <label>Asunto</label> */}
-            {/* <input type="text" name="message_subject" /> */}
-            <label>Mensaje</label>
-            <textarea name="message" />
-            <input className='pointer' type="submit" value="Enviar" />
-        </form>
+        <div>
+            {message && (
+            <div className="modal">
+                <div className="modal-content">
+                <span className="close" onClick={() => setMessage('')}>&times;</span>
+                <p>{message}</p>
+                </div>
+            </div>
+            )}
+            <form className='form' ref={form} onSubmit={sendEmail}>
+                <label>Nombre</label>
+                <input type="text" name="user_name" />
+                <label>Compañia</label>
+                <input type="text" name="company_name" />
+                <label>Email</label>
+                <input type="email" name="user_email" />
+                <label>Teléfono</label>
+                <input type="number" name="user_phone" />
+                <label>Mensaje</label>
+                <textarea name="message" />
+                <input className='pointer' type="submit" value="Enviar"/>
+            </form>
+        </div>
     )
 }
