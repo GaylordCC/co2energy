84244ce0e046121245835397dbdd92c954ca5964
GaylordCC <gcarricaballero3@hotmail.com>
2023-11-02 16:30:28 -0500

    Ajust the responsive mode, and add animation to the mobile-menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,6 +6,8 @@ import Logo from "../logo/Logo";
 const BodyHeader = () => {
     const [showNavbarMobile, setShowNavbarMobile] = useState(false);
     const [isHamburgerToggled, setIsHamburgerToggled] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+    const [key, setKey] = useState(0);
 
     const handleGoStart = () => {
         window.location.href = '/';
@@ -35,26 +37,34 @@ const BodyHeader = () => {
         if (navToggle) {
             navToggle.addEventListener('click', toggleHamburger);
         }
-    }, []);
+
+        if (!isOpen) {
+            setKey(0);
+        }
+    },  [isOpen]);
 
     const handleShowNavbar = () => {
       setShowNavbarMobile(!showNavbarMobile);
       setIsHamburgerToggled(!isHamburgerToggled);
-    //   const mobileMenu = document.querySelector(".top-bar-mobile");
-    //   mobileMenu.classList.toggle("menu-active");
+
+      setIsOpen(!isOpen);
+      setKey(prev => prev + 1);
     };
 
     const handleAbutUs = () => {
         setShowNavbarMobile(false);
+        setIsHamburgerToggled(false);
         window.location.replace("/#AU");
     }
 
     const handleService = () => {
         setShowNavbarMobile(false);
+        setIsHamburgerToggled(false);
         window.location.replace("/#Serv");
     }
     const handleContact = () => {
         setShowNavbarMobile(false);
+        setIsHamburgerToggled(false);
         window.location.replace("/#Cont");
     }
 
@@ -64,7 +74,7 @@ const BodyHeader = () => {
             <div className='logo-name' onClick={handleGoStart}>
                 <Logo />
             </div>
-            <button className='menu-icon' onClick={handleShowNavbar}>
+            <button className={`menu-icon ${isOpen ? 'is-active' : ''}`} onClick={handleShowNavbar}>
                 <a className={`nav-toggle ${isHamburgerToggled ? 'x' : ''}`}>
                     <span className="bar"></span>
                     <span className="bar"></span>
@@ -72,65 +82,29 @@ const BodyHeader = () => {
                 </a>
             </button>
             { showNavbarMobile && (
-                <div  onFocus={()=> setShowNavbarMobile(false)} className='top-bar-mobile'>
-                    <ul>
-                        <li className='start' onClick={handleGoStart}>
-                            <a>Inicio</a>
-                        </li>
-                        <li className='AboutUs' onClick={handleAbutUs}>
-                            <a>
-                                <h4 className='font-bar'><span> Nosotros </span></h4>
-                            </a>
-                        </li>
-                        <li className='Services' onClick={handleService}>
-                            <a>
-                                <h4 className='font-bar'><span> Servicios </span></h4>
-                            </a>
-                        </li>
-                        <li className='ods' onClick={handleOds}>
-                            <a >
-                                <h4 className='font-bar'><span> ODS </span></h4>
-                            </a>
-                        </li>
-                        <li className='Contact' onClick={handleContact}>
-                            <a>
-                                <h4 className='font-bar'><span> Contactanos </span></h4>
-                            </a>
-                        </li>
-                    </ul>
-                    {/* <div  className='start' onClick={handleGoStart}> Inicio </div>
-                    <div className='AboutUs' onClick={handleAbutUs}>
-                        <h4 className='font-bar'><span> Nosotros </span></h4>
-                    </div>
-                    <div className='Services' onClick={handleService}>
-                        <h4 className='font-bar'><span> Servicios </span></h4>
-                    </div>
-                    <div className='ods' onClick={handleOds}>
-                        <h4 className='font-bar'><span> ODS </span></h4>
-                    </div>
-                    <div className='Contact' onClick={handleContact}>
-                        <h4 className='font-bar'><span> Contactanos </span></h4>
-                    </div> */}
+                <div className={`mobile-menu ${setShowNavbarMobile ? 'is-open' : ''}`} key={key}>
+                    <a className="menu-item" onClick={handleGoStart}><p className='iten-font'>Inicio</p></a>
+                    <a className="menu-item" onClick={handleAbutUs}><p className='iten-font'>Nosotros</p></a>
+                    <a className="menu-item" onClick={handleService}><p className='iten-font'>Servicios</p></a>
+                    <a className="menu-item" onClick={handleOds}><p className='iten-font'>ODS</p></a>
+                    <a className="menu-item" onClick={handleContact}><p className='iten-font'>Contactanos</p></a>
                 </div>
             )}
-            { showNavbarMobile == false && (
-                <div id ="menu-items" className='top-bar-desktop'>
-                    <div className='start start-hover' onClick={handleGoStart}> Inicio </div>
-                    <div className='AboutUs' onClick={handleAbutUs}>
-                        <h4 className='font-bar'><span> Nosotros </span></h4>
-                    </div>
-                    <div className='Services' onClick={handleService}>
-                        <h4 className='font-bar'><span> Servicios </span></h4>
-                    </div>
-                    <div className='ods' onClick={handleOds}>
-                        <h4 className='font-bar'><span> ODS </span></h4>
-                    </div>
-                    <div className='Contact' onClick={handleContact}>
-                        <h4 className='font-bar'><span> Contactanos </span></h4>
-                    </div>
-                </div>   
-            ) }
-           
+            <div id ="menu-items" className='top-bar-desktop'>
+                <div className='start start-hover' onClick={handleGoStart}> Inicio </div>
+                <div className='AboutUs' onClick={handleAbutUs}>
+                    <h4 className='font-bar'><span> Nosotros </span></h4>
+                </div>
+                <div className='Services' onClick={handleService}>
+                    <h4 className='font-bar'><span> Servicios </span></h4>
+                </div>
+                <div className='ods' onClick={handleOds}>
+                    <h4 className='font-bar'><span> ODS </span></h4>
+                </div>
+                <div className='Contact' onClick={handleContact}>
+                    <h4 className='font-bar'><span> Contactanos </span></h4>
+                </div>
+            </div>     
         </div>
     )
 }
